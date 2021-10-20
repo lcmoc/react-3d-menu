c4491b636da0074a34333e89b669e8f6416a359f
Luca Mock <luca.mock@ringieraxelspringer.ch>
2021-10-20 19:27:28 +0200

    removed logo

--- src/components/Triangle/index.js
+++ src/components/Triangle/index.js
@@ -1,4 +1,3 @@
-import Logo from "../../assets/Logo.png";
 import Fläche from "./components/Fläche/index";
 import DescriptionTriangle from "./components/DescriptionTriangle/index";
 
@@ -6,9 +5,6 @@ const Triangle = () => {
   return (
     <>
       <div className="">
-        <div className="flex fixed top-0 left-0">
-          <img src={Logo} alt="Logo" className="h-40" />
-        </div>
         <div className="flex flex-row items-center justify-center mt-14">
           <div className="w-full flex items-center justify-center">
             <h2 className="text-blue-400 text-4xl">Dreieck Berechnungen</h2>
