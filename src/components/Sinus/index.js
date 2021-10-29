5a6fc059168354b68b864fa2e90965c5a5d827ec
Luca Mock <luca.mock@ringieraxelspringer.ch>
2021-10-29 15:57:19 +0200

    added sinus cosinus tan

--- src/components/Sinus/index.js
+++ src/components/Sinus/index.js
@@ -1,7 +1,10 @@
+import SinCosTanComponents from "../../SinCosTanComponents";
+
 const Sinus = () => {
     return (
       <div className="flex flex-col items-center justify-center mt-10">
         <h2 className="text-blue-400 text-4xl">Sinus</h2>
+        <SinCosTanComponents />
       </div>
     );
   };
