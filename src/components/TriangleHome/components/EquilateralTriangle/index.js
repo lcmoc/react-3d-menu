ca39bce998a5ac1c3f9d1ac57d4032df95219f83
Luca Mock <luca.mock@ringieraxelspringer.ch>
2021-11-14 20:17:51 +0100

    style improvements

--- src/components/TriangleHome/components/EquilateralTriangle/index.js
+++ src/components/TriangleHome/components/EquilateralTriangle/index.js
@@ -22,7 +22,7 @@ const EquilateralTriangle = () => {
   const handleSide = (event) => {
     const value = event.target.value;
     const name = event.target.name;
-    const hight = ((value / 2) * Math.sqrt(3));
+    const hight = (value / 2) * Math.sqrt(3);
 
     if (name && [nameSideA, nameSideB, nameSideC].includes(name)) {
       setSideC(value);
@@ -33,7 +33,7 @@ const EquilateralTriangle = () => {
       setArea((value * hight) / 2);
     } else if (name === nameScope) {
       const sideLength = Math.round(value / 3);
-      const currentHight = hight / 3
+      const currentHight = hight / 3;
       setScope(value);
       setSideC(sideLength);
       setSideB(sideLength);
@@ -41,20 +41,20 @@ const EquilateralTriangle = () => {
       setHight(currentHight);
       setArea((sideLength * currentHight) / 2);
     } else if (name === nameArea) {
-      const sideLength = Math.sqrt(value * 4 / Math.sqrt(3));
-      setArea(value) 
+      const sideLength = Math.sqrt((value * 4) / Math.sqrt(3));
+      setArea(value);
       setSideC(sideLength);
       setSideB(sideLength);
       setSideA(sideLength);
-      setHight(Math.sqrt(3) / 2 * sideLength);
-      setScope(sideLength * 3)
+      setHight((Math.sqrt(3) / 2) * sideLength);
+      setScope(sideLength * 3);
     } else if (name === nameHight) {
-      const sideLength = value / Math.sqrt(3) * 2;
-      setHight(value)
+      const sideLength = (value / Math.sqrt(3)) * 2;
+      setHight(value);
       setSideC(sideLength);
       setSideB(sideLength);
       setSideA(sideLength);
-      setScope(sideLength * 3)
+      setScope(sideLength * 3);
       setArea((sideLength * value) / 2);
     }
   };
@@ -63,49 +63,58 @@ const EquilateralTriangle = () => {
     <div className="flex items-center justify-center flex-col">
       <div className="Triangle mt-32 mb-20">
         <input
-          type="text"
+          type="number"
           name="inputRadiusOne"
           className="InputRadiusOne bg-transparent w-10 text-center"
           value={radiusValue}
           disabled
         />
         <input
-          type="text"
+          type="number"
           name="inputRadiusTwo"
           className="InputRadiusTwo bg-transparent w-10 text-center"
           value={radiusValue}
           disabled
         />
         <input
-          type="text"
+          type="number"
           name="inputRadiusThree"
           className="InputRadiusThree bg-transparent w-10 text-center"
           value={radiusValue}
           disabled
         />
+        <label htmlFor="sideB" className="InputSideC text-center">
+          <input
+            type="number"
+            name="sideC"
+            className="border border-gray-500 bg-transparent focus:border-transparent w-24 text-center rounded"
+            onChange={(event) => handleSide(event)}
+            value={sideC}
+          />
+          <p className="text-lg text-gray-500 font-bold">Seite c</p>
+        </label>
+        <label htmlFor="sideB" className="InputSideB text-center">
+          <input
+            type="number"
+            name="sideB"
+            className="border border-gray-500 bg-transparent focus:border-transparent w-24 text-center rounded appearance-none"
+            onChange={(event) => handleSide(event)}
+            value={sideB}
+          />
+          <p className="text-lg text-gray-500 font-bold">Seite b</p>
+        </label>
+        <label htmlFor="sideA" className="InputSideA text-center">
+          <input
+            type="number"
+            name="sideA"
+            className="border border-gray-500 bg-transparent focus:border-transparent w-24 text-center rounded"
+            onChange={(event) => handleSide(event)}
+            value={sideA}
+          />
+          <p className="text-lg text-gray-500 font-bold">Seite a</p>
+        </label>
         <input
-          type="text"
-          name="sideC"
-          className="SideC border border-gray-500 bg-transparent focus:border-transparent w-24 text-center rounded"
-          onChange={(event) => handleSide(event)}
-          value={sideC}
-        />
-        <input
-          type="text"
-          name="sideB"
-          className="SideB border border-gray-500 bg-transparent focus:border-transparent w-24 text-center rounded"
-          onChange={(event) => handleSide(event)}
-          value={sideB}
-        />
-        <input
-          type="text"
-          name="sideA"
-          className="SideA border border-gray-500 bg-transparent focus:border-transparent w-24 text-center rounded"
-          onChange={(event) => handleSide(event)}
-          value={sideA}
-        />
-        <input
-          type="text"
+          type="number"
           name="hight"
           className="Hight bg-transparent focus:border-transparent w-24 text-center"
           onChange={(event) => handleSide(event)}
@@ -120,7 +129,7 @@ const EquilateralTriangle = () => {
         <p className="text-lg text-gray-500 font-bold pr-5">
           Umfang U =
           <input
-            type="text"
+            type="number"
             name="scope"
             className="bg-transparent focus:border-transparent w-14 text-start p-1"
             onChange={(event) => handleSide(event)}
@@ -130,7 +139,7 @@ const EquilateralTriangle = () => {
         <p className="text-lg text-gray-500 font-bold">
           Fläche A =
           <input
-            type="text"
+            type="number"
             name="area"
             className="bg-transparent focus:border-transparent text-start w-14 p-1"
             onChange={(event) => handleSide(event)}
