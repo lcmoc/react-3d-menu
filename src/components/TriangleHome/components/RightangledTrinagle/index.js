2a9228f2491a9f309424bc2c022f991af01336f2
Luca Mock <luca.mock@ringieraxelspringer.ch>
2021-11-17 20:19:32 +0100

    added area & scope to rightangled triangle

--- src/components/TriangleHome/components/RightangledTrinagle/index.js
+++ src/components/TriangleHome/components/RightangledTrinagle/index.js
@@ -60,9 +60,9 @@ const RightAngledTriangle = () => {
   };
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center flex-col">
       <div className="border-black relative mt-20">
-        <div className="Triangle mt-40 mb-40"></div>
+        <div className="Triangle mt-40 mb-24"></div>
         <input
           type="number"
           name="RightAngledRadiusOne"
@@ -127,6 +127,28 @@ const RightAngledTriangle = () => {
         <p className="CornerA text-2xl text-gray-500 font-bold">A</p>
         <p className="CornerB text-2xl text-gray-500 font-bold">B</p>
         <p className="CornerC text-2xl text-gray-500 font-bold">C</p>
+      </div>
+      <div className="flex items-center justify-center mb-32">
+        <p className="text-lg text-gray-500 font-bold pr-5">
+          Umfang U =
+          <input
+            type="number"
+            name="scope"
+            className="bg-transparent focus:border-transparent w-14 text-start p-1"
+            onChange={(event) => handleSide(event)}
+            value={scope}
+          />
+        </p>
+        <p className="text-lg text-gray-500 font-bold">
+          Fläche A =
+          <input
+            type="number"
+            name="area"
+            className="bg-transparent focus:border-transparent text-start w-14 p-1"
+            onChange={(event) => handleSide(event)}
+            value={area}
+          />
+        </p>
       </div>
     </div>
   );
