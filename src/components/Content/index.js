82eb9927b861d3349975b3b2eeeb446133906620
Luca Mock <luca.mock@ringieraxelspringer.ch>
2021-11-25 03:06:32 +0100

    added switch for triangle calculations with or without desc

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -5,14 +5,15 @@ import Home from "../Home/index";
 import IsoscelesTriangle from "../TriangleHome/components/IsoscelesTriangle";
 import RightAngledTriangle from "../TriangleHome/components/RightangledTrinagle";
 import Sinus from "../Sinus/index";
+import Triangle from "../Triangle/index"
 import TriangleHome from "../TriangleHome/index";
 
-const Content = () => {
+const Content = ({isOn}) => {
   return (
     <>
       <Router>
         <Route exact path="/" component={Home} />
-        <Route path="/dreieck" component={TriangleHome} />
+        <Route path="/dreieck" component={isOn ? Triangle : TriangleHome} />
         <Route path="/sinus" component={Sinus} />
         <Route path="/gleichseitigesdreieck" component={EquilateralTriangle} />
         <Route path="/rechtwinklig" component={RightAngledTriangle} />
